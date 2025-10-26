24d281ffdbb1064f9b8c4b8179f068380e0d4426
Emmanuel Diaz <emmanueldiaz707@gmail.com>
2025-10-26 16:47:38 -0300

    crear ejercicio 1

---
+++ nivel1/lib/ej1.exs
@@ -0,0 +1,8 @@
+defmodule Ej1 do
+  # 1.	Defina una función que calcule el cuadrado de un número.
+  def cuadrado(nro) do
+    nro * nro
+  end
+end
+
+  IO.puts("El cuadrado de 9 es #{Ej1.cuadrado(9)}")
