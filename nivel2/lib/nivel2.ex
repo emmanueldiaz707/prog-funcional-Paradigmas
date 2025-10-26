80dd8bde9dde132744cca008fe3d3518c3c2ee49
Emmanuel Diaz <emmanueldiaz707@gmail.com>
2025-10-26 16:44:38 -0300

    crear proyecto nivel 2

---
+++ nivel2/lib/nivel2.ex
@@ -0,0 +1,18 @@
+defmodule Nivel2 do
+  @moduledoc """
+  Documentation for `Nivel2`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Nivel2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ nivel2/test/nivel2_test.exs
@@ -0,0 +1,8 @@
+defmodule Nivel2Test do
+  use ExUnit.Case
+  doctest Nivel2
+
+  test "greets the world" do
+    assert Nivel2.hello() == :world
+  end
+end
