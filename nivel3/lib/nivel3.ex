eb6d1b65e3d1b3e295b0d107ce7dc9f5ca3923f8
Emmanuel Diaz <emmanueldiaz707@gmail.com>
2025-10-29 21:26:06 -0300

    acortar consignas nivel3

--- nivel3/lib/nivel3.ex
+++ nivel3/lib/nivel3.ex
@@ -1,83 +1,85 @@
 defmodule Nivel3 do
 
 
-  # 19.	Escriba una función que calcule el i-ésimo número perfecto (los números perfectos son aquellos que son iguales a la suma de sus divisores.
+  # 19.	Función que calcule el i-ésimo número perfecto
+    # (los números perfectos son aquellos que son iguales a la suma de sus divisores.
 
 
 
 
-  # 20.	Escriba una función que calcule los n primeros números primos y los devuelva en una lista.
+  # 20.	Función que calcule los n primeros números primos y los devuelva en una lista.
 
 
 
 
-  # 21.	Escriba una función que determine la Varianza de una lista de números: Varianza = Sumatoria (Xi - Media)2 / (n - 1). Siendo Xi cada uno de los n elementos de la lista.
+  # 21.	Función que determine la Varianza de una lista de números:
+    # Varianza = Sumatoria (Xi - Media)2 / (n - 1). Siendo Xi cada uno de los n elementos de la lista.
 
 
 
 
-  # 22.	Escriba una función que calcule la Moda de una lista de números (el número que más se repite).
+  # 22.	Función que calcule la Moda de una lista de números (el número que más se repite).
 
 
 
 
-  # 23.	Devuelva la cantidad de números que contiene una lista.
+  # 23.	Función que devuelva la cantidad de números que contiene una lista.
 
 
 
 
-  # 24.	Realice una función que transforme un binario, expresado a través de una lista de {0,1}, en decimal.
+  # 24.	Función que transforme un binario, expresado a través de una lista de {0,1}, en decimal.
 
 
 
 
-  # 25.	Realice una función que sume dos números binarios, expresados a través de dos listas de {0,1}.
+  # 25.	Función que sume dos números binarios, expresados a través de dos listas de {0,1}.
 
 
 
 
-  # 26.	Realice una función que transforme un entero decimal a binario, expresado como lista de {0,1}.
+  # 26.	Función que transforme un entero decimal a binario, expresado como lista de {0,1}.
 
 
 
 
-  # 27.	Escriba una función llamada “Palíndromo” que indique si una lista es Palíndromo (capicúa).
+  # 27.	Función llamada “Palíndromo” que indique si una lista es Palíndromo (capicúa).
 
 
 
 
-  # 28.	Escriba una función que determine si una lista de {0,1}, donde cada 0 representa un paréntesis que abre y cada 1 un paréntesis que cierra, está balanceada (es decir, que cada 0 tiene su 1 correspondiente de acuerdo a las reglas que siguen los paréntesis). Ej: (001101) está balanceada y (1001010) no lo está.
+  # 28.	Función que determine si una lista de {0,1}, donde cada 0 representa un paréntesis que abre y cada 1 un paréntesis que cierra, está balanceada (es decir, que cada 0 tiene su 1 correspondiente de acuerdo a las reglas que siguen los paréntesis). Ej: (001101) está balanceada y (1001010) no lo está.
 
 
 
 
-  # 29.	Defina una función que tome como entrada una lista de números entre el 1 y el 3, y devuelva un par ordenado con el número que mayor cantidad de veces aparece en la lista original y su frecuencia.
+  # 29.	Función que tome como entrada una lista de números entre el 1 y el 3, y devuelva un par ordenado con el número que mayor cantidad de veces aparece en la lista original y su frecuencia.
 
 
 
 
-  # 30.	Realice un programa que, dado un número N, calcule el mayor valor de la serie de Fibonacci que sea menor que N. La serie está dada por: 0, 1, 1, 2, 3, 5, 8, 13, ... ; donde los  primeros números son 0 y 1, y los demás se calculan como la suma de los dos anteriores.
+  # 30.	Función que,dado un número N, calcule el mayor valor de la serie de Fibonacci que sea menor que N. La serie está dada por: 0, 1, 1, 2, 3, 5, 8, 13, ... ; donde los  primeros números son 0 y 1, y los demás se calculan como la suma de los dos anteriores.
 
 
 
 
-  # 31.	Escriba una función llamada “intercalar” que intercale dos listas ordenadas manteniendo el orden definido por las mismas.
+  # 31.	Función “intercalar” que intercale dos listas ordenadas manteniendo el orden definido por las mismas.
 
 
 
 
-  # 32.	Escriba una función que tome una lista y elimine las repeticiones adyacentes de sus elementos.
+  # 32.	Función que tome una lista y elimine las repeticiones adyacentes de sus elementos.
 
 
 
 
-  # 33.	Escriba una función que tome un número y una lista conteniendo los coeficientes de un polinomio y devuelva el resultado del mismo reemplazando el número por la variable:
+  # 33.	Función que tome un número y una lista conteniendo los coeficientes de un polinomio y devuelva el resultado del mismo reemplazando el número por la variable:
     # Ej. para (2 5 4 50 3) y 3 el polinomio en x es  2 + 5 x + 4 x2 + 50 x3 + 3 x4 ,  donde x=3.
 
 
 
 
-  # 34.	Dadas dos listas de números defina una función devuelva una tercera, cuyos elementos sean resultado del siguiente cálculo:
+  # 34.	Dadas dos listas de números Función devuelva una tercera, cuyos elementos sean resultado del siguiente cálculo:
     # Parámetros: L1, L2 listas de números.
     # Resultado: L3 lista de números
     # L3(1) = L1(1)*L2(1) + L1(1)*L2(2) + ... + L1(1)*L2(N)
@@ -97,18 +99,18 @@ defmodule Nivel3 do
 
 
 
-  # 36.	Escriba una función que tome una lista como entrada y genere una nueva lista con la primera mitad de la lista original como segundo elemento y la segunda mitad como primer elemento.
+  # 36.	Función que tome una lista como entrada y genere una nueva lista con la primera mitad de la lista original como segundo elemento y la segunda mitad como primer elemento.
     # (x1, x2, x3, x4, x5, x6)  -->  ( (x4, x5, x6), (x1, x2, x3) ).
 
 
 
 
-  # 37.	Escriba una función que tome dos listas y determine si la secuencia de elementos de la primera se encuentra dentro de la segunda.
+  # 37.	Función que tome dos listas y determine si la secuencia de elementos de la primera se encuentra dentro de la segunda.
 
 
 
 
-  # 38.	Escriba una función que tome como entrada tres listas y determine si la tercera se puede formar a partir de los elementos de las dos primeras.
+  # 38.	Función que tome como entrada tres listas y determine si la tercera se puede formar a partir de los elementos de las dos primeras.
 
 
 
