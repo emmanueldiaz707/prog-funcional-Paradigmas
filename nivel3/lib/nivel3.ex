cd9d711393cffd3ed5d72e16f3a64bbc09ed66e1
Emmanuel Diaz <emmanueldiaz707@gmail.com>
2025-10-29 21:17:37 -0300

    crear proyecto nivel3

---
+++ nivel3/lib/nivel3.ex
@@ -0,0 +1,18 @@
+defmodule Nivel3 do
+  @moduledoc """
+  Documentation for `Nivel3`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Nivel3.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ nivel3/test/nivel3_test.exs
@@ -0,0 +1,8 @@
+defmodule Nivel3Test do
+  use ExUnit.Case
+  doctest Nivel3
+
+  test "greets the world" do
+    assert Nivel3.hello() == :world
+  end
+end
