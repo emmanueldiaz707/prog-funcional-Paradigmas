485deb6d91fc9d3c96c12828aba80a0eb10dc88a
Emmanuel Diaz <emmanueldiaz707@gmail.com>
2025-12-01 08:53:15 -0300

    crear proyecto nivel 4

---
+++ nivel4/lib/nivel4.ex
@@ -0,0 +1,27 @@
+defmodule Nivel4 do
+
+  # 39. Escriba una función llamada "Cantidad-de" que toma como argumentos una lista y una condición (función), y  devuelve la cantidad de elementos de la lista que cumplen con dicha condición.
+
+
+
+
+  # 40. Defina una función que tome una lista de números y una condición (función) como parámetros y devuelva la sumatoria de los elementos que cumplen dicha condición.
+
+
+
+
+  # 41. Defina una función llamada “Select” que devuelva la lista de elementos que cumplen con una determinada condición.
+
+
+
+
+  # 42. Defina una función llamada “Map” o “Collect” que devuelva la lista de los resultados de aplicar una función que se pasa como parámetro a cada elemento de la lista de entrada.
+
+
+
+
+  # 43. Escriba una función llamada “intercalar-según” que tome dos listas y una función como entrada, y construya una nueva lista resultado de intercalar las dos primeras en el orden establecido por la función (es decir, que la función se aplica a los dos elementos que se comparan en cada momento para determinar cuál es el mayor).
+
+
+
+end
---
+++ nivel4/test/nivel4_test.exs
@@ -0,0 +1,8 @@
+defmodule Nivel4Test do
+  use ExUnit.Case
+  doctest Nivel4
+
+  test "greets the world" do
+    assert Nivel4.hello() == :world
+  end
+end
