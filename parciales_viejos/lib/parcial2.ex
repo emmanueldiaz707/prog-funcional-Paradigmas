e5f5dcf568f3c6062407e2973b48863eebb48182
Emmanuel Diaz <emmanueldiaz707@gmail.com>
2025-12-03 11:35:58 -0300

    parcial 2 ejercicio 2

--- parciales_viejos/lib/parcial2.ex
+++ parciales_viejos/lib/parcial2.ex
@@ -18,4 +18,55 @@ defmodule Parcial2 do
     [eliminarP(x, p) | eliminarPSublistas(xs, p)]
   end
 
+
+
+  # Escriba la función que tome como entrada una lista de pares ordenados y una lista de números (sin sublistas),
+  # y devuelva otra lista que contenga una sublista por cada par, donde cada sublista incluya los elementos de
+  # la lista de números que pertenecen a la secuencia aritmética definida por dicho par.
+  # Cada par (A, S) define una secuencia que comienza en A y aumenta en S unidades cada paso (A, A+S, A+2S, ...).
+  # Si S es negativo, la sublista correspondiente debe estar vacía.
+  # Cada número puede aparecer en múltiples sublistas o en ninguna.
+  # Los elementos en cada sublista deben mantener el orden de aparición en la lista original.
+
+  # Lista de pares = [[3, 2], [6, 0], [12, 3], [7, 1]]
+  # Lista de números = [4, 6, 4, 10, 3, 2, 5]
+  # Resultado = [[3, 5], [6], [], [10]]
+
+  def maximo(lista), do: maximo(lista, hd(lista))
+  def maximo([], mayor), do: mayor
+  def maximo([x|xs], mayor) do
+    cond do
+      x > mayor -> maximo(xs, x)
+      true -> maximo(xs, mayor)
+    end
+  end
+
+  def incluye([], _elem), do: false
+  def incluye([x|xs], elem) do
+    cond do
+      x == elem -> true
+      true -> incluye(xs, elem)
+    end
+  end
+
+  def secArit([a, s], maximo, nros) do
+    cond do
+      s < 0 -> []
+      a <= maximo ->
+        incl = incluye(nros,a)
+        cond do
+          incl and s>0 -> [a | secArit([a+s, s], maximo, nros)]
+          incl -> [a]
+          s==0 -> []
+          true -> secArit([a+s, s], maximo, nros)
+        end
+      true -> []
+    end
+  end
+
+  def listaSecArit([], _nros), do: []
+  def listaSecArit([x|xs], nros) do
+    [secArit(x, maximo(nros), nros) | listaSecArit(xs, nros)]
+  end
+
 end
--- parciales_viejos/test/parcial2_test.exs
+++ parciales_viejos/test/parcial2_test.exs
@@ -2,9 +2,40 @@ defmodule Parcial2Test do
   use ExUnit.Case
   doctest Parcial2
 
-  test "Parcial 1, ejercicio 1: " do
+  test "Parcial 2, ejercicio 1" do
     assert Parcial2.eliminarP([1,2,3,4,5,6,7,8], 5) == [1,2,3,4,6,7,8]
     assert Parcial2.eliminarPSublistas([[3, 8, 4, 2, 3], [1, 6, 3, 9, 8], [12, 15]], 3) == [[3, 8, 2, 3], [1, 6, 9, 8], [12, 15]]
+  end
+
+
+
+  test "Parcial 2, ejercicio 2: Máximo" do
+    assert Parcial2.maximo([1,7,9,5,1]) == 9
+    assert Parcial2.maximo([1]) == 1
+  end
+  test "Parcial 2, ejercicio 2: Incluye" do
+    assert Parcial2.incluye([1,2,3,4], 5) == false
+    assert Parcial2.incluye([1,2,3,4], 2) == true
+  end
+  test "Parcial 2, ejercicio 2: Secuencia aritmética" do
+    assert Parcial2.secArit([3,2], 20, [4,6,4,10,3,2,5,18,19]) == [3,5,19]
+    assert Parcial2.secArit([3,-2], 20, [4,6,4,10,3,2,5,18,19]) == []
+  end
+  test "Parcial 2, ejercicio 2: Secuencia aritmética para [3,2]" do
+    assert Parcial2.secArit([3, 2], 10, [4, 6, 4, 10, 3, 2, 5]) == [3,5]
+  end
+  test "Parcial 2, ejercicio 2: Secuencia aritmética para [6,0]" do
+    assert Parcial2.secArit([6, 0], 10, [4, 6, 4, 10, 3, 2, 5]) == [6]
+  end
+  test "Parcial 2, ejercicio 2: Secuencia aritmética para [12,3]" do
+    assert Parcial2.secArit([12, 3], 10, [4, 6, 4, 10, 3, 2, 5]) == []
+  end
+  test "Parcial 2, ejercicio 2: Secuencia aritmética para [7,1]" do
+    assert Parcial2.secArit([7, 1], 10, [4, 6, 4, 10, 3, 2, 5]) == [10]
+  end
+
+  test "Parcial 2, ejercicio 2" do
+    assert Parcial2.listaSecArit([[3, 2], [6, 0], [12, 3], [7, 1]], [4, 6, 4, 10, 3, 2, 5]) == [[3, 5], [6], [], [10]]
   end
 
 end
