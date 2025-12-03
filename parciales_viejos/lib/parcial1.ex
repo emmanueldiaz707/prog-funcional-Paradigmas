5e54f6a8450cfaa642a494459c914b45835b9602
Emmanuel Diaz <emmanueldiaz707@gmail.com>
2025-12-03 09:38:57 -0300

    parcial 1 ejercicio 2

--- parciales_viejos/lib/parcial1.ex
+++ parciales_viejos/lib/parcial1.ex
@@ -11,4 +11,57 @@ defmodule Parcial1 do
   end
   def listaElemMulN([_x|xs], n, cont), do: listaElemMulN(xs, n, cont-1)
 
+
+
+
+  # Dada una lista con sublistas L, devolver una lista donde el iésimo elemento
+  # representa la sumatoria de los números contenidos en nivel i de profunidad de L.
+  # Si no hay elementos numéricos en un nivel específico, ese nivel tendrá valor 0 en la lista resultante.
+
+  # L = [1, [2, 3], [[4, 5], 6], [7, [8, [9]]]]
+  # Resultado = [1, 18, 17, 9]
+
+  # Sumatoria de elementos de una lista
+  def sumatoria([]), do: 0
+  def sumatoria([x|xs]) do
+    x + sumatoria(xs)
+  end
+
+  # Máxima profundidad de lista
+  def profMax([]), do: 1
+  def profMax([x|xs]) do
+    cond do
+      is_list(x) ->
+        profCab = 1 + profMax(x)
+        profCol = profMax(xs)
+        cond do
+          profCab > profCol -> profCab
+          true -> profCol
+        end
+      true -> profMax(xs)
+    end
+  end
+
+
+  # Obtener lista con todos los elementos de nivel de profundidad n
+  def listaNivelN([], _n), do: []
+  def listaNivelN([x|xs], 1) do
+    cond do
+      is_list(x) -> listaNivelN(xs, 1)
+      true -> [x | listaNivelN(xs, 1)]
+    end
+  end
+  def listaNivelN([x|xs], n) do
+    cond do
+      is_list(x) -> listaNivelN(x, n-1) ++ listaNivelN(xs, n)
+      true -> listaNivelN(xs, n)
+    end
+  end
+
+  # Construir lista con sumatorias
+  def sumatoriaProfI(lista), do: sumatoriaProfI(lista, 1, profMax(lista))
+  def sumatoriaProfI(_lista, _i, 0), do: []
+  def sumatoriaProfI(lista, i, p) do
+    [sumatoria(listaNivelN(lista, i)) | sumatoriaProfI(lista, i+1, p-1)]
+  end
 end
--- parciales_viejos/test/parcial1_test.exs
+++ parciales_viejos/test/parcial1_test.exs
@@ -6,6 +6,21 @@ defmodule Parcial1Test do
     assert Parcial1.listaElemMulN([4, 5, 9, 6, 12, 8, 6, 5, 4, 3, 2, 3, 5, 10], 3) == [9, 8, 4, 3]
   end
 
+  test "Parcial 1 - Ejercicio 2: lista con iésimo elemento = sumatoria de elementos de nivel de profundiad i" do
+    assert Parcial1.listaNivelN([1, [2, 3], [[4, 5], 6], [7, [8, [9]]]], 1) == [1]
+    assert Parcial1.listaNivelN([1, [2, 3], [[4, 5], 6], [7, [8, [9]]]], 2) == [2,3,6,7]
+    assert Parcial1.listaNivelN([1, [2, 3], [[4, 5], 6], [7, [8, [9]]]], 3) == [4,5,8]
+    assert Parcial1.listaNivelN([1, [2, 3], [[4, 5], 6], [7, [8, [9]]]], 4) == [9]
 
 
+    assert Parcial1.profMax([1,2,3,4]) == 1
+    assert Parcial1.profMax([1,[1,2,3],3,[1,2,3]]) == 2
+    assert Parcial1.profMax([[1,2],2,[1,2,3,[1,2]],4]) == 3
+    assert Parcial1.profMax([1,[1,[1,2,[1,2,3]],3],3,4]) == 4
+    assert Parcial1.profMax([1,2,3,4, [[[1]]]]) == 4
+  #  assert Parcial1.profMax([]) == 0
+
+    assert Parcial1.sumatoriaProfI([1, [2, 3], [[4, 5], 6], [7, [8, [9]]]]) == [1, 18, 17, 9]
+    assert Parcial1.sumatoriaProfI([1, [2, 3], [[4, 5], 6], [7, [8, [9, [[5]]]]]]) == [1, 18, 17, 9, 0, 5]
+  end
 end
